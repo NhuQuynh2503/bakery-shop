514a9961341c83fd2c52a9d3b1e0a993073e2ccd
NhuQuynh2503 <nnnhuquynh03@gmail.com>
2024-08-10 22:08:53 +0700

    add project

---
+++ script.js
@@ -0,0 +1,25 @@
+let navbar = document.querySelector('.navbar');
+let menu = document.querySelector('#menu-bar');
+let section=document.querySelectorAll('section');
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+  }
+  for (let i = 0; i < section.length; i++) {
+    section[i].onclick=function(){
+      menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    };
+    
+  }
+  // window.addEventListener('scroll',function(){
+  //   if(window.pageYOffset>50){
+
+  //   }
+  // })
+  window.onscroll=()=>{
+    if(window.pageYOffset>50){
+      menu.classList.remove('fa-times');
+      navbar.classList.remove('active');
+    }
+  }
